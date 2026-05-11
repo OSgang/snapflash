8db2adf3d559c52b6b014dcba0a0b95ebaee8a45
ngTanPhuc <nguyentanphuc020505@gmail.com>
2026-05-11 22:00:03 +0700

    Fix: fix bug where the loading algorithm skips half of the dictionary, doesn't save the last word and reference to currentDefinitions

--- backend/src/main/kotlin/com/osgang/backend/service/DictionaryService.kt
+++ backend/src/main/kotlin/com/osgang/backend/service/DictionaryService.kt
@@ -27,23 +27,29 @@ class DictionaryService (
                     .filter { it.isNotBlank() }
                     .filterNot { it.startsWith('*') }  // skip the word class
                     .filterNot { it.startsWith('=') }  // skip the usage example
-                    .filterNot { it.startsWith("@00") }  // skip the dictionary metadata
+                    .filterNot { it.startsWith("@00") }  // skip the dictionary's metadata
                     .forEach { line ->
                         // If line start with '@' -> flush previous entry into dictionary
                         //                        -> delete currentWord, currentDefinitions
                         //                        -> create new currentWord
-                        if (line.startsWith('@')) {
+                        if (line.startsWith('@')) {  // if there's a new word, add the previous word to the dictionary and save the new word to currentWord
                             if (currentWord.isNotEmpty()) {
-                                dictionary[currentWord] = currentDefinitions;
-                                currentWord = "";
+                                dictionary[currentWord] = currentDefinitions.toList();
                                 currentDefinitions.clear();
-                            } else {
-                                currentWord = line.substringAfter('@').substringBefore(' ');
                             }
+
+                            // Add new word
+                            currentWord = line.substringAfter('@').substringBefore(' ');
+
                         } else {  // if line starts with '-' -> definition
                             currentDefinitions.add(line.substringAfter('-'))
                         }
                 }
+            }
+
+            // Add the last word because the algo above won't save it when it reaches the EOF
+            if (currentWord.isNotEmpty()) {
+                dictionary[currentWord] = currentDefinitions.toList();
             }
 
             println("\uD83D\uDCD6 Dictionary loaded with ${dictionary.size} words")
@@ -51,5 +57,9 @@ class DictionaryService (
             println("Error while loading dictionary: ${e.message}")
             // TODO: Throw a RunTime exception
         }
+    }
+
+    fun lookup(word: String): List<String> {
+        return dictionary[word].orEmpty();
     }
 }
